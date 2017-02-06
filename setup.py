b925496518bbbebc433b23bc261a7f5f9efe1ecb
Robert Joyal <rjoyal@gmail.com>
2017-02-05 21:44:45 -0500

    Version 1.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='domain_shared_contacts_client',
-    version='0.1.0',
+    version='1.0.3',
     description="A Python client to CRUD Google Domain Shared Contacts",
     long_description=readme + '\n\n' + history,
     author="Robert Joyal",
