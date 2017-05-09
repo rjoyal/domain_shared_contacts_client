63c19a4a74958409d980c2ce48d9efcdbda7f28c
Gary Reynolds <gary.reynolds@internationaltouch.org>
2017-05-09 22:17:30 +1000

    Update the command line interface to use groups

--- domain_shared_contacts_client/cli.py
+++ domain_shared_contacts_client/cli.py
@@ -4,75 +4,112 @@ import json
 import os.path
 
 import click
-
 import contacts_helper
 import domain_shared_contacts_client
 
-actions = ['list', 'create', 'read', 'update', 'delete']
 
-
-def validate_file(ctx, param, value):
+@click.group()
+@click.argument('domain')
+@click.argument('admin')
+@click.argument('credentials', type=click.Path(exists=True))
+@click.pass_context
+def cli(ctx, domain, admin, credentials):
     """
-    Check if the value parameter points to an existing file
-    Use validate_required_file to validate that the parameter is present
+    Command line utility to interact with the Shared Contacts API.
     """
-    if value is None:
-        return None
-    if not os.path.isfile(value):
-        # TODO how to get parameter name in error message
-        raise click.BadParameter('%s should point to a existing file: %s not found' % (param.human_readable_name, value))
-    return value
+    ctx.obj['domain'] = domain
+    ctx.obj['admin'] = admin
+    ctx.obj['credentials'] = credentials
 
 
-def validate_required_file(ctx, param, value):
-    if value is None:
-        raise click.BadParameter('%s are required' % param.human_readable_name)
-    return validate_file(ctx, param, value)
+@cli.command()
+@click.option('--indent', '-i', type=int, default=None)
+@click.option('--sort/--no-sort', default=True)
+@click.option('--output', '-o', type=click.File('w'), default='-')
+@click.pass_context
+def list(ctx, indent, sort, output):
+    """
+    all contacts
+    """
+    client = domain_shared_contacts_client.Client(**ctx.obj)
+    contacts = client.get_contacts()
+    json.dump(contacts, output,
+              default=contacts_helper.convert_contacts,
+              sort_keys=sort, indent=indent)
 
 
-def validate_action(ctx, param, value):
-    """ Check if the action is supported """
-    if value not in actions:
-        raise click.BadParameter('invalid action %s' % value)
-    return value
+@cli.command()
+@click.argument('contact', type=click.Path(exists=True))
+@click.option('--indent', '-i', type=int, default=None)
+@click.option('--sort/--no-sort', default=True)
+@click.option('--output', '-o', type=click.File('w'), default='-')
+@click.pass_context
+def create(ctx, contact, indent, sort, output):
+    """
+    a single contact
+    """
+    client = domain_shared_contacts_client.Client(**ctx.obj)
+    contact = client.create_contact(contact)
+    json.dump(contact, output,
+              default=contacts_helper.convert_contacts,
+              sort_keys=sort, indent=indent)
 
 
-@click.command()
-@click.option('--domain', help='Your Google Apps domain (e.g. example.com')
-@click.option('--admin', help='Your domain admin account email (e.g. admin@example.com)')
-@click.option('--credentials', help='JSON credentials for your service account',
-              callback=validate_required_file)
-@click.option('--action', help='One of the following actions: list, create, read, update, delete',
-              callback=validate_action)
-@click.option('--contact_details', help='A JSON file containing a new contact record',
-              callback=validate_file)
-@click.option('--id', help='The ID of a contact to read, update or delete')
-def main(domain, admin, credentials, action, contact_details, id):
-    """Console script for domain_shared_contacts_client"""
-    client = domain_shared_contacts_client.Client(domain=domain, admin=admin,
-                                                  credentials=credentials)
-    if action == 'list':
-        contacts = client.get_contacts()
-        print(json.dumps(contacts, default=contacts_helper.convert_contacts,
-                         sort_keys=True, indent=4))
-    elif action == 'create':
-        if contact_details is None:
-            raise click.UsageError('Please provide contact_details')
-        saved_contact = client.create_contact(contact_details)
-        print(json.dumps(saved_contact))
-    elif action == 'read':
-        contact = client.read_contact(id)
-        print(json.dumps(contact, default=contacts_helper.convert_contact,
-                         sort_keys=True, indent=4))
-    elif action == 'update':
-        if contact_details is None:
-            raise click.UsageError('Please provide contact_details')
-        contact = client.update_contact(id, contact_details)
-        print(json.dumps(contact, default=contacts_helper.convert_contact,
-                         sort_keys=True, indent=4))
-    elif action == 'delete':
-        result = client.delete_contact(id)
-        print(json.dumps(result))
+@cli.command()
+@click.argument('id')
+@click.option('--indent', '-i', type=int, default=None)
+@click.option('--sort/--no-sort', default=True)
+@click.option('--output', '-o', type=click.File('w'), default='-')
+@click.pass_context
+def read(ctx, id, indent, sort, output):
+    """
+    a single contact
+    """
+    client = domain_shared_contacts_client.Client(**ctx.obj)
+    key = 'http://www.google.com/m8/feeds/contacts/%s/base/%s'
+    contact = client.read_contact(key % (ctx.obj['domain'], id))
+    json.dump(contact, output,
+              default=contacts_helper.convert_contact,
+              sort_keys=sort, indent=indent)
+
+
+@cli.command()
+@click.argument('contact', type=click.Path(exists=True))
+@click.option('--indent', '-i', type=int, default=None)
+@click.option('--sort/--no-sort', default=True)
+@click.option('--output', '-o', type=click.File('w'), default='-')
+@click.pass_context
+def update(ctx, contact, indent, sort, output):
+    """
+    a single contact
+    """
+    client = domain_shared_contacts_client.Client(**ctx.obj)
+    with open(contact) as fp:
+        data = json.load(fp)
+    contact = client.update_contact(data['id'], contact)
+    json.dump(contact, output,
+              default=contacts_helper.convert_contact,
+              sort_keys=sort, indent=indent)
+
+
+@cli.command()
+@click.argument('id')
+@click.option('--indent', '-i', type=int, default=None)
+@click.option('--sort/--no-sort', default=True)
+@click.option('--output', '-o', type=click.File('w'), default='-')
+@click.pass_context
+def delete(ctx, id, indent, sort, output):
+    """
+    a single contact
+    """
+    client = domain_shared_contacts_client.Client(**ctx.obj)
+    key = 'http://www.google.com/m8/feeds/contacts/%s/base/%s'
+    result = client.delete_contact(key % (ctx.obj['domain'], id))
+    json.dump(result, output, sort_keys=sort, indent=indent)
+
+
+def main():
+    cli(obj={})
 
 
 if __name__ == "__main__":
